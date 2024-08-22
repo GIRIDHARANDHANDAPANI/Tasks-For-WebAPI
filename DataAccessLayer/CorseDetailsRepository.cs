34b690e755798d1d19ca3f46de419c10e820fbf7
SATHESHKUMAR S <satheshk.sakthivel@gmail.com>
2024-08-22 14:58:22 +0530

    Creating WebAPI for CourseDetails Module

---
+++ DataAccessLayer/CorseDetailsRepository.cs
@@ -0,0 +1,107 @@
+﻿using DataAccessLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+
+namespace DataAccessLayer
+{
+    public class CorseDetailsRepository
+    {
+        string connectionString = "server=DESKTOP-8VD1A1F\\SQLEXPRESS;database=Course;user Id =sa;password=Anaiyaan@123;";
+        SqlConnection con = null;
+        public CorseDetailsRepository()
+        {
+            con = new SqlConnection(connectionString);
+        }
+
+        public List<CourseDetails> SelectALLStudent()
+        {
+            try
+            {
+                var selectQuery = $"exec_SelectAllStudents";
+                con.Open();
+                List<CourseDetails> result = con.Query<CourseDetails>(selectQuery).ToList();
+                con.Close();
+
+                return result;
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public CourseDetails SelectUserByName(string username)
+        {
+            try
+            {
+                var selectQuery = $"exec_SelectByName_'{username}'";
+                con.Open();
+                CourseDetails result = con.QueryFirstOrDefault<CourseDetails>(selectQuery);
+                con.Close();
+
+                return result;
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public void RegisterUser(CourseDetails reg)
+        {
+            try
+            {
+                var insertQuery = $"exec_InsertUser_'{reg.Name}','{reg.InstitutionName}',{reg.EnquiryNumber},'{reg.Startdate}','{reg.Enddate}',{reg.Fees}";
+                con.Open();
+                con.Execute(insertQuery);
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public void UpdateUser(CourseDetails reg)
+        {
+            try
+            {
+                var updateQuery = $"exec_UpdateUser_{reg.CourseDetailsID},'{reg.Name}','{reg.InstitutionName}',{reg.EnquiryNumber},'{reg.Startdate}','{reg.Enddate}',{reg.Fees}";
+                //var connectionString = "server=DESKTOP-8VD1A1F\\SQLEXPRESS;database=batch9;user Id =sa;password=Anaiyaan@123;";
+
+                con.Open();
+                con.Execute(updateQuery);
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+        public void DeleteUser(long regId)
+        {
+            try
+            {
+                var updateQuery = $"exec_DeleteUser_{regId}";
+                con.Open();
+                con.Execute(updateQuery);
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ DataAccessLayer/Entity/CourseDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entity
+{
+  public  class CourseDetails
+    {
+        public long CourseDetailsID { get; set; }
+        public string Name { get; set; }
+        public string InstitutionName { get; set; }
+        public long EnquiryNumber { get; set; }
+        public int Duration { get; set; }
+        public DateTime Startdate { get; set; }
+        public DateTime Enddate { get; set; }
+        public int Fees { get; set; }
+    }
+}
